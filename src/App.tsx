804ac5c6f843bd202ac061ce0be9547a3e5f3929
LLmoskk <1398145450@qq.com>
2023-07-01 19:10:05 +0800

    feat: 小修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { defineComponent, ref } from "vue";
 import "./App.css";
-import Logo from "./meditation.png";
+import Logo from "./assets/meditation.png";
+import GithubLogo from "./assets/github-mark.png";
 
 interface Message {
   question: string;
@@ -31,6 +32,10 @@ export default defineComponent({
         "如来！",
         "如来？",
         "如来~",
+        "如来了吗？",
+        "如来吗 ~",
+        "他真来了吗?",
+        "来没来?",
         "诶，如来",
       ];
       const answer =
@@ -49,6 +54,9 @@ export default defineComponent({
         <div class="header">
           <img class="logo" src={Logo} />
           <h1>RuLaiChat</h1>
+          <a href="https://github.com/LLmoskk/rulaiChat" target="_blank">
+            <img class="github" src={GithubLogo} />
+          </a>
         </div>
         <div class="content" ref={contentRef}>
           {messages.length > 0 &&
